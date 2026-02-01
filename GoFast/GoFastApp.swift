f0f38c23d926e24e42e3ad9444af02b2cc3fada5
adrianrdguez <adrianrdgueznavas@gmail.com>
2026-02-01 23:08:44 +0700

    Initial Commit

---
+++ GoFast/GoFastApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoFastApp.swift
+//  GoFast
+//
+//  Created by Ucademy on 1/2/26.
+//
+
+import SwiftUI
+
+@main
+struct GoFastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GoFastTests/GoFastTests.swift
@@ -0,0 +1,17 @@
+//
+//  GoFastTests.swift
+//  GoFastTests
+//
+//  Created by Ucademy on 1/2/26.
+//
+
+import Testing
+@testable import GoFast
+
+struct GoFastTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
